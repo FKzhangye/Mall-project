51f17c5a7b6b9c89225b867abffa0856ea66c778
FKzhang <2127816215@qq.com>
2017-09-11 17:25:43 +0800

    change

---
+++ js/register.js
@@ -0,0 +1,39 @@
+var oUsername = document.querySelector("input[name=username]");
+var oPassword = document.querySelector("input[name=password]");
+var oVerify = document.querySelector("input[name=verify]");
+var oButton = document.querySelector("button");
+
+var oUser = document.querySelector("#errorUsername");
+var oPass = document.querySelector("#errorPassworld");
+var oVer = document.querySelector("#errorVerification");
+
+
+oUsername.onblur = function(){
+	if(!(/^\w{3,16}$/.test(this.value))){
+		oUser.style.display = "block";
+	}
+}
+oPassword.onblur = function(){
+	if(!(/^\w{6,16}$/.test(this.value))){
+		oPass.style.display = "block";
+	}
+}
+oVerify.onblur = function(){
+	if(!(oPassword.value === oVerify.value)){
+		oVer.style.display = "block";
+	}
+}
+
+oButton.onclick = function(){
+    //先进行js校验用户名和密码符合规范，js校验通过
+    //请求ajax进行校验用户名是否可用,当这个校验通过，就可以请求注册接口
+      myajax.post('http://h6.duchengjiu.top/shop/api_user.php',
+    {
+      status: 'register',
+      username: oUsername.value,
+      password: oPassword.value
+    }, function(err, responseText){
+      var json = JSON.parse(responseText);
+      alert(json.message);
+    });
+  }
